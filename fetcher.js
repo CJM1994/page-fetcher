3c7a5fb8fa4ed9704d32283fc9a4d6e6b7534097
CJM1994 <connorjohnmullin@gmail.com>
2022-02-16 19:09:07 -0800

    fswriteFile to index.js

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,7 @@
 const URL = process.argv[2];
 const FILE_PATH = process.argv[3];
-
+const fs = require('fs');
+let rawFile = '';
 
 const fetchPage = function () {
 
@@ -8,5 +9,35 @@ const fetchPage = function () {
 
 }
 
-console.log(URL);
-console.log(FILE_PATH);
+const net = require('net');
+const connection = net.createConnection({
+  host: 'www.example.edu',
+  port: 80
+})
+connection.setEncoding('UTF8');
+
+connection.write('GET / HTTP/1.1\r\n');
+connection.write(`Host: www.example.edu\r\n`);
+connection.write('\r\n');
+
+connection.on('data', (data) => {
+  rawFile = data;
+
+  fs.writeFile('./index.html', rawFile, err => {
+
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+  })
+
+})
+
+
+
+// console.log(URL);
+// console.log(FILE_PATH);
+
+// TESTS
+// node fetcher.js www.example.edu ./
